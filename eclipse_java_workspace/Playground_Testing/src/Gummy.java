0195981bf9dc39755a35f19e8370891653e98b5a
hectorrios <hectorrios@10.117.104.180>
2019-03-26 18:27:09 -0700

    # WARNING: head commit changed in the meantime
    Sending Demo

---
+++ eclipse_java_workspace/Playground_Testing/src/Gummy.java
@@ -0,0 +1,13 @@
+//Hector Rios. Student ID: 220205545
+
+public class Gummy {
+    public Gummy()
+    {
+    	System.out.println("Hello Gummy Class Instance..\n");
+    }
+    public static void main(String [] args)
+    {
+    	//Gummy Bear = new Gummy();
+    	System.out.println("Hi, This is the main defintion..\n");
+    }
+}
